cca01f6e6d9dc783daad65664b379b35cd40d651
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-05-16 12:10:11 +0200

    creazione edit

--- net-il-mio-fotoalbum/Controllers/HomeController.cs
+++ net-il-mio-fotoalbum/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MessagePack;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Models;
 using System.Data;
 using System.Diagnostics;
@@ -91,6 +93,66 @@ namespace net_il_mio_fotoalbum.Controllers
                 ctx.Photos.Add(photo);
                 ctx.SaveChanges();
                 return RedirectToAction("Admin");
+            }
+
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Edit(int id)
+        {
+            using (AlbumContext ctx = new AlbumContext())
+            {
+                Photo pt = ctx.Photos.Where(p => p.Id == id).Include(p => p.Categories).FirstOrDefault();  
+                FormPhotoCategory form = new FormPhotoCategory();
+                var categoriesList = ctx.Categories.ToList();
+
+                List<SelectListItem> listCategories = new List<SelectListItem>();
+                foreach (Category cat in categoriesList)
+                {
+                    listCategories.Add(new SelectListItem() { Text = cat.Name, Value = cat.Id.ToString(), Selected = pt.Categories.Any(c => c.Id == cat.Id) });
+                }
+                form.Photo = pt;
+                form.Categories = listCategories;
+                return View(form);
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Edit(int id,FormPhotoCategory form)
+        {
+            using (AlbumContext ctx = new AlbumContext())
+            {
+                if (!ModelState.IsValid)
+                {
+                   
+        
+                    var categoriesList = ctx.Categories.ToList();
+
+                    List<SelectListItem> listCategories = new List<SelectListItem>();
+                    foreach (Category cat in categoriesList)
+                    {
+                        listCategories.Add(new SelectListItem() { Text = cat.Name, Value = cat.Id.ToString(), });
+                    }
+                    form.Categories = listCategories;
+                    return View(form);
+
+                }
+                Photo pt = ctx.Photos.Where(p => p.Id == id).Include(p => p.Categories).FirstOrDefault();
+                pt.Title = form.Photo.Title;
+                pt.Description = form.Photo.Description;
+                pt.Image = form.Photo.Image;
+                pt.Visible = form.Photo.Visible;
+                pt.Categories.Clear();
+                foreach (var categoryid in form.SelectedCategories)
+                {
+                    int intcategoryid = int.Parse(categoryid);
+                    var category = ctx.Categories.Where(c => c.Id == intcategoryid).FirstOrDefault();
+                    pt.Categories.Add(category);
+                }
+                ctx.Photos.Update(pt);
+                ctx.SaveChanges();
+                return RedirectToAction("Admin");
+                
+
             }
 
         }
